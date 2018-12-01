2050b16ab51821c0bd6b8546c9fc1d55594166ff
Yamkovoi Alexei <yamkooi.a@gmail.com>
2018-12-02 01:10:51 +0200

    Redirect and JWT works

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     res.status(404);
     res.json({
       status: 404,
-      message: 'Route not found!',
+      message: 'Route not found! server',
     });
     next();
   });
